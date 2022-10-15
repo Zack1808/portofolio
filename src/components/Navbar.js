36a15a5900d917e7058144f9513a3032a7e0446c
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2022-10-15 20:01:21 +0200

    Added some more styling to the Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
-import Hamburger from 'hamburger-react'
+import { Divide as Hamburger } from 'hamburger-react'
 
 // Importing the style file
 import '../css/Navbar.css';
@@ -9,17 +9,19 @@ import '../css/Navbar.css';
 const Navbar = ({ urls }) => {
 
     const [toggle, setToggle] = useState(false)
+    let n = .2;
 
     return (
         <nav className='navbar'>
-            <Link to="/"  className="logo"><h2>Logo</h2></Link>
+            <Link to="/"  className="logo" onClick={() => setToggle(false)}><h2>Logo</h2></Link>
             <div className="navbar-container">
                 <div className="navbar-hamburger">
-                    <Hamburger color="white" onToggle={t => setToggle(t)} />
+                    <Hamburger color="white" toggle={setToggle} toggled={toggle} duration={.8} easing="ease-in-out" rounded/>
                 </div>
                 <div className={toggle ? "navbar-link-list-active navbar-link-list" : "navbar-link-list"}>
                 {urls.map(url => {
-                    return <Link to={url.link} key={url.title} className="navbar-link">{url.title}</Link>
+                    n += .17
+                    return <Link to={url.link} key={url.title} className="navbar-link" style={{ transition: toggle && `opacity .5s ${n}s`}} onClick={() => setToggle(false)}>{url.title}</Link>
                 })}
                 </div>
             </div>
