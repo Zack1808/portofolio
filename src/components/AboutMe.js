d1143870ca79dd5554536011d2d7bc08b540e642
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2022-10-19 07:57:11 +0200

    Modified the AboutMe component

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,4 +1,5 @@
 import React, { useRef, useEffect }from 'react'
+import { useInView } from 'react-intersection-observer';
 
 // Importing the costume made components
 import Hero from './Hero'
@@ -16,6 +17,8 @@ import tvz from '../img/tvz.png'
 // Text for the components 
 const hero_text = "I am a self thaught React.js web developer. Find out more down below"
 const WHO_AM_I = "Hi!  My name is Jean-Pierre and I am a 22 year-old self-taught web developer. My web development journey started in 2018, when I was first introduced to HTML and CSS. Ever since I've started upgrading my knowledge-base with JS and PHP until I've met React.js. I've been working with React for 2 years now altogether, building fun little project when I had the time. Unfortunately, not all of them are listed in GitHub, since I used those little projects to improve my skills (Visually, they are a mess)."
+const TSRB = "My web development days actually started here. At first, I wanted to become a game developer, which was the main reason I applied to this school. At the time I was applying, it was considered to be one of the highest ranking technical schools in Croatia. We started learning about C and since I had no technical background, it was very hard. As I got to my third year, I was not sure that game development was for me. And that is when I was introduced to HTML and CSS. At first glance, it was plain boring working with it. However, after the introduction of JS, jQuery and PHP, it became more interesting. Later on, I would start watching tutorials on how to make some fun apps, where I stumbled across React.js."
+const TVZ = "Afer two years I managed to get a sufficient amount of knowledge of React.js, using tutorials and online courses to get proficient with it. However, since without a diploma, at least in Croatia, it is extremly difficult to find a job in Software development, which is why I applied to TVZ (University of applied science in Zagreb, or in Croatian, Tehničko Veleučilište Zagreb). The education is currently still ongoing."
 
 // Creating the AboutMe component
 const AboutMe = () => {
@@ -28,6 +31,10 @@ const AboutMe = () => {
         top.current?.scrollIntoView({ top: 0});
     }, [])
 
+    const [ref, inView] = useInView({
+        threshold: 0.1
+    })
+
     // Function that will scroll to the segment list
     const scrollToSegments = () => {
         segmentRef.current?.scrollIntoView({ behavior: 'smooth' })
@@ -38,8 +45,9 @@ const AboutMe = () => {
             <Hero buttonText="Read more" imageHalf={person} title="Hi, I'm Jean-Pierre" text={hero_text} buttonClick={scrollToSegments}/>
             <div className="segment-list-container" ref={segmentRef}>
                 <Segment image={code} flipped title='Who am I?' text={WHO_AM_I} />
-                <Segment image={ruder} />
-                <Segment image={tvz} flipped/>
+                <h1 className={`edu ${inView && "visible"}`} ref={ref}>Education</h1>
+                <Segment image={ruder} title="Tehnička škola Ruđera Boškovića" text={TSRB}/>
+                <Segment image={tvz} flipped title="Tehničko Veleučilište Zagreb" text={TVZ} />
             </div>
         </div>
     )
